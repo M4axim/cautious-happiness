33b348f508fe23e06462d14901d0b5ce9287e2d0
M4aXim <maksim.malbasa@gmail.com>
2021-11-12 21:35:48 +0100

    WOW

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,24 +1,11 @@
-const { Client, Intents } = require('discord.js');
-const { token } = require('./config.json');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { clientId, guildId, token } = require('./config.json');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const commands = [];
 
-client.once('ready', () => {
-	console.log('Ready!');
-});
+const rest = new REST({ version: '9' }).setToken(token);
 
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
-
-	const { commandName } = interaction;
-
-	if (commandName === 'ping') {
-		await interaction.reply('Pong!');
-	} else if (commandName === 'server') {
-		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
-	} else if (commandName === 'user') {
-		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
-	}
-});
-
-client.login(token);
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+	.then(() => console.log('Successfully registered application commands.'))
+	.catch(console.error);
